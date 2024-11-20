1747e170198cb480fc3bedb9dbfd95dea5be0fb6
hezonglianheng <976410976@qq.com>
2024-11-20 15:40:15 +0800

    修改脚本，可以去除以”;“表示的测试文件注释

--- remove_space.py
+++ remove_space.py
@@ -2,16 +2,19 @@
 # 去除测试文件中的原有空格
 
 from pathlib import Path
+import re
 
 def main():
     file_path: str = input("请输入文件路径：")
-    with Path(file_path).open('r', encoding='utf8') as f:
+    path = Path(file_path)
+    with Path(file_path).open('r', encoding='gbk') as f:
         lines = f.readlines()
 
+    lines = [re.sub(r';.*', '', i) for i in lines] # 去除文件中的注释
     lines = ["".join(i.split()) for i in lines if len(i.split()) > 0]
     lines = [i + '\n' for i in lines]
 
-    with Path(file_path).open('w', encoding='utf8') as f:
+    with path.with_name(path.stem + '_new' + path.suffix).open('w', encoding='utf8') as f:
         f.writelines(lines)
 
 if __name__ == '__main__':
